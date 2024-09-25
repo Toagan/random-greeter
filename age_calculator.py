b83bddf36147e8f0284eda715589a7a34660cece
Toagan <76570345+Toagan@users.noreply.github.com>
2024-09-25 18:06:35 +0000

    Initial commit: Add age calculator script

---
+++ age_calculator.py
@@ -0,0 +1,10 @@
+import datetime
+
+def ageCalculator(y, m, d):
+    today = datetime.datetime.now().date()
+    dob = datetime.date(y, m, d)
+    age = int((today-dob).days / 365.25)
+    print(age)
+
+# Example usage
+ageCalculator(1998, 9, 3)
